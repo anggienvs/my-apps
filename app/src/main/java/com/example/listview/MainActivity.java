3f11a597180166330792cf229fad777f1d01290e
Anggie Noviansyah <anggie.n@students.amikom.ac.id>
2020-11-02 10:17:21 +0700

    List View

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] movie ={"Bad Boys for Life","The Old Guard","Raised by Wolf","Elite","The Walking Dead : World Beyond","Artemis Fowl","Black Box","Riverdale","Scary Movie 5","Star Trek : Discovery","Paddington 2","District 9"};
+    private ListView lv_movie;
+    private ArrayAdapter<String> movieAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        movieAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,movie);
+        lv_movie = findViewById(R.id.lv_movie);
+        lv_movie.setAdapter(movieAdapter);
+    }
+}
